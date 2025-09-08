a4e44650cc2ad2107a54525600b560d26f33771f
danielsantosbr255 <danielsantosbr255@gmail.com>
2025-09-08 00:56:16 -0300

    feat(debounce): solve 2627 - Debounce
    Introduced a new `debounce` function that delays the execution of a provided function until after a specified wait time has elapsed since the last time it was invoked. An example demonstrates its usage, showing how multiple calls are canceled and only the last call is logged after the delay.

---
+++ javascript-30-days/debounce.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function (fn, t) {
+  let timeOut;
+  return function (...args) {
+    clearTimeout(timeOut);
+    timeOut = setTimeout(() => fn(...args), t);
+  };
+};
+
+const log = debounce(console.log, 100);
+log("Hello1"); // cancelled
+log("Hello2"); // cancelled
+log("Hello3"); // Logged at t=100ms
